9dba5ed9df44d0ecdc52c392a83e6ea437e7f4ae
Evgeniy Ivanov <timeescapenow@gmail.com>
2020-05-29 21:35:32 +0300

    Add audio

--- src/main/java/ru/sfedu/ictis/museumapp/ui/exhibits/ExhibitsFragment.kt
+++ src/main/java/ru/sfedu/ictis/museumapp/ui/exhibits/ExhibitsFragment.kt
@@ -1,9 +1,12 @@
 package ru.sfedu.ictis.museumapp.ui.exhibits
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import ru.sfedu.ictis.museumapp.R
@@ -11,6 +14,10 @@ import ru.sfedu.ictis.museumapp.R
 class ExhibitsFragment : Fragment() {
 
     private lateinit var exhibitsViewModel: ExhibitsViewModel
+
+    var mPlayer: MediaPlayer? = null
+    var id: Int? = 0
+    var play_button: ImageView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,4 +29,39 @@ class ExhibitsFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_exhibits, container, false)
         return root
     }
+
+    override fun onStart() {
+        initAudioPlayer()
+        super.onStart()
+    }
+
+    fun initAudioPlayer() {
+        when (id) {
+            1011 -> {
+                mPlayer = MediaPlayer.create(context, R.raw.exhibit1011)
+            }
+            1014 -> {
+                mPlayer = MediaPlayer.create(context, R.raw.exhibit1014)
+            }
+            else -> {
+                mPlayer = MediaPlayer.create(context, R.raw.undefined)
+            }
+        }
+        play_button = this.activity?.findViewById(R.id.play_btn) as ImageView
+        mPlayer?.setOnPreparedListener {
+            println("Ready to GO")
+        }
+
+        play_button?.setOnClickListener {
+            mPlayer?.start()
+        }
+
+        /*
+        stop_button.setOnClickListener {
+            mPlayer?.pause()
+        }
+         */
+    }
+
+
 }
